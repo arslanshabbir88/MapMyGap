c23b4e4b071eaf818bb422c54a2e6efec48b5901
arslanshabbir88 <arslan.shabbir@gmail.com>
2025-08-16 21:52:32 -0400

    Reword instruction text to be more user-friendly: 'Select a control family/CSF function to analyze'

--- src/Analyzer.jsx
+++ src/Analyzer.jsx
@@ -1037,7 +1037,7 @@ function Analyzer({ onNavigateHome }) {
                       <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                       </svg>
-                      <span>Choose one control family for focused analysis</span>
+                      <span>Select a control family to analyze</span>
                     </div>
                   </div>
                   
@@ -1134,7 +1134,7 @@ function Analyzer({ onNavigateHome }) {
                       <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
                         <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
                       </svg>
-                      <span>Choose one CSF function for focused analysis</span>
+                      <span>Select a CSF function to analyze</span>
                     </div>
                   </div>
                   
