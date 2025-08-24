542df95bf992b62ab2becbb54bb9af80be09b2cf
arslanshabbir88 <arslan.shabbir@gmail.com>
2025-08-24 18:08:01 -0400

    Fix module.exports to export default for ES module compatibility

--- api/analyze.js
+++ api/analyze.js
@@ -5639,7 +5639,7 @@ allFrameworks.NIST_800_63B = {
   ]
 };
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
